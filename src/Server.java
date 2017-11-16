4de2878d1ad1861451fb6dea327b93af000eb7a3
tjewell <tjewell@myocv.com>
2017-11-15 23:03:00 -0600

    Tested some server talking

--- src/Server.java
+++ src/Server.java
@@ -8,26 +8,39 @@ public class Server {
 
     public static void main(String[] args) throws IOException {
 
+        int serverPort;
+
         if (args.length < 1) {
             System.err.println("Parameters required: serverPort");
             System.exit(1);
         }
 
+        try {
+            serverPort = Integer.parseInt(args[0]);
+        } catch (Exception e) {
+            System.err.println("Parameters required: serverPort (must be an integer)");
+            System.exit(1);
+        }
+
+        serverPort = Integer.parseInt(args[0]);
+
+        // Create a new dgram
+        DatagramSocket packetSocket = new DatagramSocket(serverPort);
+
+        byte[] receivedMessage = new byte[MAX_BUFF_LEN - 7];
+
+        DatagramPacket receivePacket = new DatagramPacket(receivedMessage, receivedMessage.length);
+        System.out.println("Waiting for client request...");
+
+        boolean clientWaiting = false;
+
+
         while (true) {
-            // Create a new dgram
-            DatagramSocket packetSocket = new DatagramSocket();
-
-            byte[] receivedMessage = new byte[MAX_BUFF_LEN - 9];
-
-            DatagramPacket receivePacket = new DatagramPacket(receivedMessage, receivedMessage.length);
-            System.out.println("Waiting for client request...");
             packetSocket.receive(receivePacket);
             System.out.println("Packet received...");
 
             byte[] response = receivePacket.getData();
             int respLen = receivePacket.getData().length;
-
-
         }
     }
 }
